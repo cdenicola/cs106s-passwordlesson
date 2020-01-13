e55cf9797e5d3ca89464846b2451cf2ffd96f070
Cooper deNicola <cdenicol@stanford.edu>
2020-01-12 23:51:55 -0800

    Password refactor

--- background.js
+++ background.js
@@ -1,8 +1,40 @@
-function VigenereHelper() {
-  var plaintext = document.getElementById("plaintext").value
-  var key = document.getElementById("key").value
-  var cipherText = Vigenere(plaintext, key)
-  ciphertext_element = document.getElementById("ciphertext")
-  ciphertext_element.style = ""
-  ciphertext_element.innerHTML = cipherText
+/* Handles submit button
+*/
+function submitPassword() {
+  var password = document.getElementById("password").value
+  var encryptedPassword = passwordEncrypter(password)
+  display_element = document.getElementById("encryptedPass")
+  display_element.style = ""
+  display_element.innerHTML = encryptedPassword
+}
+
+/* mapping transforms some (lowercase) letter input
+   to a corrosponding number
+   EX: mapping['a'] => 0
+       mapping['b'] => 1
+             ...
+       mapping['z'] => 25
+*/
+var mapping = {
+  "a": 0,  "b": 1,  "c": 2,  "d": 3,  "e": 4,
+  "f": 5,  "g": 6,  "h": 7,  "i": 8,  "j": 9,
+  "k": 10, "l": 11, "m": 12, "n": 13, "o": 14,
+  "p": 15, "q": 16, "r": 17, "s": 18, "t": 19,
+  "u": 20, "v": 21, "w": 22, "x": 23, "y": 24,
+  "z": 25
+}
+
+/* Creates an array containg (lowercase) alphabet letters
+   alphabet => ["a", "b", "c", ..., "z"]
+   Q: what is alphabet[14]? => "o"
+*/
+var alphabet = Object.keys(mapping);
+
+
+// alphabet[mapping['a']] = 'a'
+
+
+function Vigenere(plaintext, key) {
+ //TODO
+ return "temporary string"
 }
